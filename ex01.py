9e1106c25de4761a1260a1ea9cd1753e93f55b4a
Michel <106674127+MichelZero@users.noreply.github.com>
2023-06-29 15:08:49 -0300

    up

--- ex01.py
+++ ex01.py
@@ -11,22 +11,22 @@
 # 1
 print("1 - Programa que imprime a quantidade de números pares de 100 até 200, incluindo-os.")
 
-valor1 = 100
-valor2 = 200
+valor1 = 100 # valor inicial
+valor2 = 200 # valor final
 
-for i in range(valor1, valor2 + 1):
-    if i % 2 == 0:
-        print(i, end=" ")
+for i in range(valor1, valor2 + 1): # +1 para incluir o valor final
+    if i % 2 == 0: # se o resto da divisão por 2 for igual a 0, então é par
+        print(i, end=" ") # end=" " para imprimir na mesma linha
         
 # 2
 print("2 - Programa que imprime a soma dos números ímpares de 100 até 200, incluindo-os.")
 
 valor1 = 100
 valor2 = 200
-somaImpar = 0
+somaImpar = 0 # variável para armazenar a soma dos números ímpares de 100 até 200
 
 for i in range(valor1, valor2 + 1):
-    if i % 2 != 0:
-        somaImpar += i
+    if i % 2 != 0: # se o resto da divisão por 2 for diferente de 0, então é ímpar
+        somaImpar += i # somaImpar = somaImpar + i
         
 print(f"A soma dos números ímpares de 100 até 200 é: {somaImpar}")
