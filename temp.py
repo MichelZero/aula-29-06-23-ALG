e9644abae0caba578ac603f23c76506e07b27459
Michel <106674127+MichelZero@users.noreply.github.com>
2023-07-05 07:22:19 -0300

    up

--- temp.py
+++ temp.py
@@ -3,27 +3,11 @@
 
 # data: 29/06/2023
 
-# fazer o programa usando o while.
 
-# 4 - Programa que calcule o fatorial de um número.
-# 0! = 1
-# 1! = 1
-# 2! = 2*1 = 2
-# 3! = 3*2*1 = 6
-# 4! = 4*3*2*1 = 24
-# 5! = 5*4*3*2*1 = 120
 
-# entrada de dados
-numero = int(input("Digite um número: "))
+lista = [1,2,3]
 
-# variável para armazenar o fatorial
-fatorial = 1
+valor1 = str(lista)
 
-# processamento
-while numero > 0:
-    print(f"{numero}*{fatorial}={numero*fatorial}")
-    fatorial *= numero # fatorial = fatorial * numero
-    numero -= 1 # numero = numero - 1
-    
-# saída de dados
-print(f"O fatorial é: {fatorial}")
+print(type(valor1))
+print(valor1[0])
